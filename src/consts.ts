5a6eedc4e869c44c760f83b7f15324466fb30096
Lewis Wang <i@lewisw.net>
2026-07-15 15:05:12 +0800

    Edit about.astro

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Lewisw';
-export const SITE_DESCRIPTION = '自言自语';
+export const SITE_DESCRIPTION = 'The personal website of Lewis Wang, focused on domains, email, Linux, networking and self-hosting.';
